9382d14d36620df5953c6a7b4db2d615ae78e951
Carlos Urbín <carlos@urbin.es>
2010-06-04 07:18:23 +0100

    Version in lua

---
+++ simon.lua
@@ -0,0 +1,225 @@
+-- class.lua
+-- Compatible with Lua 5.1 (not 5.0).
+function class(base, init)
+   local c = {}    -- a new class instance
+   if not init and type(base) == 'function' then
+      init = base
+      base = nil
+   elseif type(base) == 'table' then
+    -- our new class is a shallow copy of the base class!
+      for i,v in pairs(base) do
+         c[i] = v
+      end
+      c._base = base
+   end
+   -- the class will be the metatable for all its objects,
+   -- and they will look up their methods in it.
+   c.__index = c
+
+   -- expose a constructor which can be called by <classname>(<args>)
+   local mt = {}
+   mt.__call = function(class_tbl, ...)
+   local obj = {}
+   setmetatable(obj,c)
+   if init then
+      init(obj,...)
+   else 
+      -- make sure that any stuff from the base class is initialized!
+      if base and base.init then
+      base.init(obj, ...)
+      end
+   end
+   return obj
+   end
+   c.init = init
+   c.is_a = function(self, klass)
+      local m = getmetatable(self)
+      while m do 
+         if m == klass then return true end
+         m = m._base
+      end
+      return false
+   end
+   setmetatable(c, mt)
+   return c
+end
+
+Game = class(function(game,level)
+	game:start_level(level)
+	game.played = false
+	game.snd = nil
+end)
+
+function Game:start_level(level)
+	self.level = level
+	self.steps = {}
+	for i = 1,level,1 do
+		self.steps[i] = math.random(4)
+	end
+  self.cursor = 1
+end
+
+function Game:restart_level()
+	self.cursor = 1
+	print("..xx..")
+end
+
+function Game:next_step()
+	return self.steps[self.cursor]
+end
+
+function Game:do_step(step)
+	if self.steps[self.cursor] == step then
+		self.cursor = self.cursor+1
+		return true
+	else
+		return false
+	end
+end
+  
+function Game:cleared()
+	return self.cursor > #self.steps
+end
+
+
+Button = class(function(btn,number)
+  btn.imgoff = elf.CreateTextureFromFile("resources/simon"..number..".png")
+  btn.imgover = elf.CreateTextureFromFile("resources/simon"..number.."over.png")
+  btn.snd = elf.LoadSound("resources/snd"..number..".ogg")
+  btn.btn = elf.CreateButton('SndBtn'..number)
+  elf.SetButtonOffTexture(btn.btn, btn.imgoff) 
+  elf.SetButtonOverTexture(btn.btn, btn.imgover) 
+  elf.SetButtonOnTexture(btn.btn, btn.imgoff)
+end)
+  
+function Button:rx()
+	return elf.GetGuiObjectSize(self.btn).x
+end
+
+function Button:ry()
+	return elf.GetGuiObjectSize(self.btn).y
+end
+
+-- set window title 
+elf.SetTitle("BlendELF Test Context")
+
+-- limit fps
+elf.SetFpsLimit(25)
+
+-- create and set a gui
+gui = elf.CreateGui()
+elf.SetGui(gui)
+
+-- elf logo
+tex = elf.CreateTextureFromFile("resources/Elf.png")
+pic = elf.CreatePicture("ELFLogo")
+elf.SetPictureTexture(pic, tex)
+elf.AddGuiObject(gui, pic)
+size = elf.GetGuiObjectSize(pic)
+elf.SetGuiObjectPosition(pic, elf.GetWindowWidth()-size.x, 0)
+
+-- create font for labels
+font = elf.CreateFontFromFile('resources/FreeSans.ttf', 26)
+
+-- New game at level one
+game = Game(1)
+
+-- tracked info
+functions = {
+	function() return "The Simon's elf under "..elf.GetVersion() end,
+  -- function() return "FPS: " .. elf.GetFps() end,
+	function() return "Level: "..game.level end,
+}
+
+-- create text list for text
+txt = elf.CreateTextList("TXTlist")
+elf.SetTextListFont(txt,font)
+elf.SetTextListSize(txt,# functions,elf.GetWindowWidth()-16)
+elf.SetGuiObjectPosition(txt, 8, 8)
+elf.AddGuiObject(gui, txt)
+
+-- create musical buttons
+coords = {{0,0},{1,0},{0,1},{1,1}}
+snds = {}
+for i = 1,4,1 do
+  table.insert(snds,Button(i))
+	local last = snds[#snds]
+  elf.SetGuiObjectPosition(last.btn, 
+    (elf.GetWindowWidth()-last:rx()*2)/2+coords[i][1]*last:rx(),
+    (elf.GetWindowHeight()-last:ry()*2)/2+coords[i][2]*last:ry())
+  elf.AddGuiObject(gui, last.btn)
+end
+
+-- create menu
+btns = {
+  {
+    name = 'ExitButton',
+    images = {"resources/btnoff.png","resources/btnover.png","resources/btnon.png"},
+    clicked = "elf.Quit()"
+  },
+  {
+    name = 'RepeatButton',
+    images = {"resources/repeatoff.png","resources/repeatover.png","resources/repeaton.png"},
+    clicked = "game:restart_level()\ngame.snd = nil\ngame.played = false"
+  }
+}
+
+for i,b in ipairs(btns) do
+  local exbtexoff  = elf.CreateTextureFromFile(b.images[1])
+  local exbtexover = elf.CreateTextureFromFile(b.images[2])
+  local exbtexon   = elf.CreateTextureFromFile(b.images[3])
+  b.elf_btn = elf.CreateButton(b.name) 
+  elf.SetButtonOffTexture(b.elf_btn, exbtexoff) 
+  elf.SetButtonOverTexture(b.elf_btn, exbtexover) 
+  elf.SetButtonOnTexture(b.elf_btn, exbtexon) 
+  local size = elf.GetGuiObjectSize(b.elf_btn)
+  elf.SetGuiObjectPosition(b.elf_btn, 
+		elf.GetWindowWidth()-size.x, 
+		elf.GetWindowHeight()-(size.y*(i+1))
+	)
+	
+	local exscr = elf.CreateScript() 
+	elf.SetScriptText(exscr, b.clicked) 
+	elf.SetGuiObjectScript(b.elf_btn, exscr) 
+  elf.AddGuiObject(gui, b.elf_btn)
+end
+
+-- main loop
+while elf.Run()==true do
+  -- uptade text info
+  elf.RemoveTextListItems( txt )
+	for i,v in ipairs(functions) do
+		elf.AddTextListItem(txt, v() )
+	end
+	
+	-- -- check mouse interection when not in playing mode
+	if game.played==true then
+  	for indx,i in ipairs(snds) do
+    	if elf.GetGuiObjectEvent(i.btn) == elf.CLICKED then
+    	  if(game:do_step(indx)==true) then
+    	    game.snd = elf.PlaySound(i.snd,1.0)
+    	    if(game:cleared()==true) then
+    	      game:start_level(game.level+1)
+    	      game.played = false
+  	      end
+        else
+          elf.PlaySound(i.snd,0.3)
+        end
+      end
+    end
+  -- play current steps first
+  else
+    if not (game.snd~=nil and elf.IsSoundPlaying(game.snd)==true) then
+      if not game:cleared()==true then
+        print(game:next_step())
+        game.snd = elf.PlaySound(snds[game:next_step()].snd,1.0)
+        game:do_step(game:next_step())
+      else
+        game:restart_level()
+        game.snd = nil
+        game.played = true
+      end
+    end  
+  end
+	
+end
